6f3ca80609ee9f89e499969e2b0d283f3c8c571f
徐峰 <xufeng1@syswin.com>
2019-06-27 09:05:02 +0800

    去掉.git文件

--- bin/create-frame-vue.js
+++ bin/create-frame-vue.js
@@ -36,9 +36,60 @@ const prompts = [
         type: 'input',
         name: 'projectPort',
         message: '请设定dev-server端口: ',
-        default: '9090'
+        default: '8080'
     }
 ];
+// 复制文件夹以及文件
+const copy = function (src,dst) {
+    let paths = fs.readdirSync(src); //同步读取当前目录
+    paths.forEach(function(path){
+        var _src=src+'/'+path;
+        var _dst=dst+'/'+path;
+        fs.stat(_src,function(err,stats){  //stats  该对象 包含文件属性
+            if(err)throw err;
+            if(stats.isFile()){ //如果是个文件则拷贝
+                let  readable=fs.createReadStream(_src);//创建读取流
+                let  writable=fs.createWriteStream(_dst);//创建写入流
+                readable.pipe(writable);
+            }else if(stats.isDirectory()){ //是目录则 递归
+                checkDirectory(_src,_dst,copy);
+            }
+        });
+    });
+}
+const checkDirectory=function(src,dst,callback){
+    if (dst.indexOf('.') > -1) {
+        let  readable=fs.createReadStream(src);//创建读取流
+        let  writable=fs.createWriteStream(dst);//创建写入流
+        readable.pipe(writable);
+    } else {
+        fs.access(dst, fs.constants.F_OK, (err) => {
+            if(err){
+                fs.mkdirSync(dst);
+                callback(src,dst);
+            }else{
+                callback(src,dst);
+            }
+        });
+    }
+};
+// 删除
+
+const _deleteFolder = function (path) { // 删除文件夹及文件
+    if (!path) return;
+    var files = [];
+    files = fs.readdirSync(path);
+    files.forEach((file,index) => {
+        var curPath = path + '/' + file;
+        if (fs.statSync(curPath).isDirectory()) { // recurse
+            _deleteFolder(curPath);
+        } else { // delete file
+            fs.unlinkSync(curPath);
+        }
+    });
+    fs.rmdirSync(path);
+}
+
 var promptParams = {};
 inquirer.prompt(prompts).then(answers => {
     promptParams = answers;
@@ -70,4 +121,7 @@ inquirer.prompt(prompts).then(answers => {
     let file = fs.readFileSync(this.combPath + '/vue.config.js', 'utf8');
     file = file.replace(new RegExp('8080', 'gm'), promptParams.projectPort);
     fs.writeFileSync(this.combPath + '/vue.config.js', file, 'utf8');
+}).then(() => {
+    _deleteFolder(this.combPath + '/.git');
+    console.log('end');
 });
